80d73e2f849e1d358f0443d5c8135743da332e19
emily.chen <emily.chen@browan.com>
2018-05-04 10:38:56 +0800

    first commit

---
+++ src/com/company/JoinMeDelegateImpl.java
@@ -0,0 +1,13 @@
+package com.company;
+
+import com.gemteks.nim.sdkinterface.JoinmeAgentDelegate;
+
+public class JoinMeDelegateImpl implements JoinmeAgentDelegate {
+
+    // 用来接收SDK层传上来的消息
+    @Override
+    public void OnRecieveMsg(String Msg) {
+        System.out.println("callback "+Msg);
+        Main.jai.sendChatCmd(Msg);
+    }
+}
---
+++ src/com/company/Main.java
@@ -0,0 +1,17 @@
+package com.company;
+
+import com.gemteks.nim.sdkinterface.JoinmeAgentInterfaceImpl;
+
+public class Main {
+
+    public static JoinmeAgentInterfaceImpl agentInterfaceImpl = null;
+
+    public static void main(String[] args) {
+	// write your code here
+
+        JoinMeDelegateImpl delegateImpl  = new JoinMeDelegateImpl();// 代理类
+        JoinmeAgentInterfaceImpl agentInterfaceImpl = new JoinmeAgentInterfaceImpl(delegateImpl);
+
+        agentInterfaceImpl.init("robottest","d5f6a3b7-5a91-4a95-9dca-9636f03d0ec2","BhNhzUEHTBGucAvNUeYlFzvBJZcaBSKttwtKkuJl", "c21f969b-5f03-433d-95e0-4f8f136e7682", "97b36bff-96ee-419d-9736-8b98ed2b603a","xZfLTnrwqy99yS/vzD1rDm94n3GeedOAMCDolS1jLjs=");
+    }
+}
